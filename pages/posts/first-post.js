ffaa94c9988e0e4fde655ecb859eaecad538c027
niranjana687 <niranjana687@gmail.com>
2021-07-16 00:40:33 +0530

    initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+
+export default function FirstPost() {
+    return (
+    <Layout>
+    <Head>
+        <title>First Post</title>
+    </Head>
+    <h1>First Post</h1>
+    <a href='/'><h2>Back to Home</h2></a>
+    
+    
+    </Layout>
+        
+        );
+}
