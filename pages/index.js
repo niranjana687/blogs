26ef247a8f19cb1828c0dfcf1bdc71e645fc9a5d
niranjana687 <niranjana687@gmail.com>
2021-07-16 18:16:53 +0530

    posts added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { getSortedPostsData } from '../lib/posts'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +21,20 @@ export default function Home() {
       <section className={utilStyles.headingMD}>
         <p>Hello! I am Niranjana</p>
         <p>I am a computer science and engineering undergraduate student from India!</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
